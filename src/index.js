3c008113dcedc2fbb1cbe4255ce1e963ee56a14d
Will Chang <william.chaang@gmail.com>
2023-07-02 19:56:27 -0600

    Basic components created

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Home from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
